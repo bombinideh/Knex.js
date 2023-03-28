4df49f076f7951e261b4c8e620bc3acc3a98b04b
Deborah Bombini <dehhbombini@gmail.com>
2023-03-28 15:56:11 -0300

    :wrench: Primeiros passos com o knex

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      port: 7768,
+      user: "postgres",
+      password: "1234567",
+      database: "knex_test",
+    },
+    pool: { min: 0, max: 7 },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+  },
+};
---
+++ src/database/migrations/20230323185845_create_table_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  await knex.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.text("username").unique().notNullable();
+
+    table.timestamp("create_at").defaultTo(knex.fn.now());
+    table.timestamp("create_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.schema.dropTable("users");
+};
